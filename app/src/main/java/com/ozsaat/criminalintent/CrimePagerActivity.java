b510b776d7106992abcf43b1ff53960fe4f54cc8
ozsaat <oss202@gmail.com>
2015-12-01 11:32:07 +0000

    11.3 Added CriminalPagerActivity

---
+++ app/src/main/java/com/ozsaat/criminalintent/CrimePagerActivity.java
@@ -0,0 +1,53 @@
+package com.ozsaat.criminalintent;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+
+import java.util.List;
+import java.util.UUID;
+
+public class CrimePagerActivity extends FragmentActivity {
+
+    private static final String EXTRA_CRIME_ID = "com.ozsaat.criminalintent.crime_id";
+
+    private ViewPager viewPager;
+    private List<Crime> crimes;
+
+    public static Intent newIntent (Context packageContext, UUID crimeId) {
+        Intent intent = new Intent(packageContext, CrimePagerActivity.class);
+        intent.putExtra(EXTRA_CRIME_ID, crimeId);
+        return intent;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime_pager);
+
+        UUID crimeId = (UUID) getIntent().getSerializableExtra(EXTRA_CRIME_ID);
+
+        viewPager = (ViewPager) findViewById(R.id.activity_crime_pager_view_pager);
+        crimes = CrimeLab.get(this).getCrimes();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        viewPager.setAdapter(new FragmentPagerAdapter(fragmentManager) {
+
+
+            @Override
+            public Fragment getItem(int position) {
+                Crime crime = crimes.get(position);
+                return CrimeFragment.newInstance(crime.getId());
+            }
+
+            @Override
+            public int getCount() {
+                return crimes.size();
+            }
+        });
+    }
+}
